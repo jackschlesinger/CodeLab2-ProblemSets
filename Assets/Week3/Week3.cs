eddff8144f8fbff8f6788452749944c538933b6d
Jack Schlesinger <jackschlesinger@gmail.com>
2019-09-20 17:02:58 -0400

    Adding week 3.

---
+++ Assets/Week3/Week3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class Week3 : MonoBehaviour
+{
+    /*
+     * Create a function that returns "true" if every entry in a string array is unique,
+     * "false" if there's a repeated string.
+     *
+     * For example:
+     *     - AllAreUnique(new string[] { "Apple", "Apple", "Chair" })  ==> would return false.
+     *     - AllAreUnique(new string[] { "Orange", "Apple", "Chair" }) ==> would return true.
+    */
+
+    private bool AllAreUnique(string[] toCheck)
+    {
+
+        return true;
+    }
+
+    /*
+     * Create a function that takes in two strings, and returns the shorter string repeated
+     * until it's the length of the longer one.
+     *
+     * For example:
+     *     - RepeatShorter("la", "songbird")     ==> returns "lalalala"
+     *     - RepeatShorter("applause", "clap")   ==> returns "clapclap"
+     *     - RepeatShorter("ba", "sheep")        ==> returns "babab"
+     * 
+    */
+    
+    private string RepeatShorter(string one, string two)
+    {
+
+        return "";
+    }
+
+    // =========================== DON'T EDIT BELOW THIS LINE =========================== //
+
+    public TextMeshProUGUI uniqueNames, repeatShorterTest;
+    private void Update()
+    {
+        uniqueNames.text = "Unique Assignment\n<align=left>\n";
+        uniqueNames.text += Success(!AllAreUnique(new string[] {"Steve", "Steve", "Abba"})) + " handles non-uniqueness.\n";
+        uniqueNames.text += Success(AllAreUnique(new string[] {"Jane", "Steve", "Abba"})) + " Handles uniqueness.\n";
+        uniqueNames.text += Success(AllAreUnique(new string[0])) + " Handles empty array.\n";
+        
+        repeatShorterTest.text = "Repeat Shorter Assignment\n<align=left>\n";
+        repeatShorterTest.text += Success(RepeatShorter("la", "songbird") == "lalalala") + " handles first shorter.\n";
+        repeatShorterTest.text += Success(RepeatShorter("applause", "clap") == "clapclap") + " Handles second shorter.\n";
+        repeatShorterTest.text += Success(RepeatShorter("ba", "sheep") == "babab") + " Handles uneven number of letters.\n";
+    }
+
+    private string Success(bool test)
+    {
+        return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
+    }
+}
---
+++ Assets/Week3/Week3Bonus.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class Week3Bonus : MonoBehaviour
+{
+    /*
+     * Create a function that determines the frequency of each object in an array.
+     *
+     * For example:
+     *     - Frequency<int>({0, 0, 0, 1, 1})
+     *         ==> dictionary with key/value pairs {[0, 3], [1, 2]}
+     * 
+     *     - Frequency<string>({"A", "A", "B", "C", "C")
+     *         ==> dictionary with key/value pairs {["A", 2], ["B", 1], ["C", 2]}
+     * 
+    */
+    
+    private Dictionary<T, int> Frequency<T>(T[] objects)
+    {
+        
+        
+        return new Dictionary<T, int>();
+    }
+
+    // =========================== DON'T EDIT BELOW THIS LINE =========================== //
+
+    public TextMeshProUGUI frequencyTests;
+
+    private void Update()
+    {
+        frequencyTests.text = "Frequency Assignment\n<align=left>\n";
+
+        var test1 = Frequency(new string[] {"A", "A", "B"});
+        
+        frequencyTests.text += Success(test1.ContainsKey("A") && test1["A"] == 2) + " Handles strings.\n";
+
+        var test2 = Frequency(new int[] {1, 1, 2, 3, 3, 3});
+
+        frequencyTests.text += Success(test2.ContainsKey(3) && test2[3] == 3) + " Handles integers.\n";
+        
+    }
+
+    private string Success(bool test)
+    {
+        return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
+    }
+}
