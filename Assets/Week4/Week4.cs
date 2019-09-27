03c700995cd0d2ff313a9b3442606a0d959ac6ff
Jack Schlesinger <jackschlesinger@gmail.com>
2019-09-27 16:16:28 -0400

    Added week 4

---
+++ Assets/Week4/Week4.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class Week4 : MonoBehaviour
+{
+    /*
+     * Create a function that returns the number of primes up to and including a given integer.
+     *
+     * For example:
+     *     - HowManyPrimes(10)     ==> returns 4 (2, 3, 5, and 7)
+     *     - HowManyPrimes(19)     ==> returns 8 (2, 3, 5, 7, 11, 13, 17 and 19)
+     *     - HowManyPrimes(30)     ==> returns 10 (2, 3, 5, 7, 11, 13, 17, 19, 23 and 29)
+    */
+
+    private int HowManyPrimes(int max)
+    {
+        
+        
+        return 0;
+    }
+
+    /*
+     * Override the "Speak" function in Pet for Dog and Cat to make dogs go "Woof" and
+     * cats go "Meow".  Also, cats don't need leashes, but dogs do.
+     * 
+    */
+
+    private abstract class Pet
+    {
+        public abstract string Speak();
+        public abstract bool NeedsLeash();
+    }
+
+    private class Dog : Pet
+    {
+        public override string Speak()
+        {
+            return "";
+        }
+
+        public override bool NeedsLeash()
+        {
+            return false;
+        }
+    }
+
+    private class Cat : Pet
+    {
+        public override string Speak()
+        {
+            return "";
+        }
+
+        public override bool NeedsLeash()
+        {
+            return false;
+        }
+    }
+    
+
+    // =========================== DON'T EDIT BELOW THIS LINE =========================== //
+
+    public TextMeshProUGUI howManyPrimes, catsAndDogs;
+    private void Update()
+    {
+        howManyPrimes.text = "How Many Primes Assignment\n<align=left>\n";
+        howManyPrimes.text += Success(HowManyPrimes(10) == 4) + " correct for first non-inclusive test.\n";
+        howManyPrimes.text += Success(HowManyPrimes(19) == 8) + " correct for first inclusive test.\n";
+        howManyPrimes.text += Success(HowManyPrimes(100) == 25) + " correct for second non-inclusive test.\n";
+        howManyPrimes.text += Success(HowManyPrimes(201) == 46) + " correct for second inclusive test.\n";
+        
+        catsAndDogs.text = "Cats and Dogs\n<align=left>\n";
+        catsAndDogs.text += Success(MakePetsSpeak(new List<Pet> { new Cat(), new Dog() }).ToLower() == "meow woof") + " speak correctly.\n";
+        catsAndDogs.text += Success(!new Cat().NeedsLeash()) + " cats don't need leashes.\n";
+        catsAndDogs.text += Success(new Dog().NeedsLeash()) + " dogs need leashes.\n";
+    }
+    
+    private string MakePetsSpeak(List<Pet> pets)
+    {
+        var toReturn = "";
+        
+        foreach (var pet in pets)
+        {
+            toReturn += pet.Speak() + " ";
+        }
+        
+        return toReturn.TrimEnd(' ');
+    }
+    
+    private string Success(bool test)
+    {
+        return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
+    }
+}
---
+++ Assets/Week4/Week4Bonus.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+public class Week4Bonus : MonoBehaviour
+{
+    /*
+     * Create a function that takes in an array of numbers and returns
+     * the sum of that fibonacci number.
+     * https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+     *
+     * For Example:
+     *     - FibonacciSum(2, 5, 9) =>
+     *         0, 1 => 1
+     *         0, 1, 1, 2, 3 =>  3
+     *         0, 1, 1, 2, 3, 5, 8, 13, 21 => 21
+     *         return 1 + 3 + 21 = 25
+     *     - FibonacciSum(11, 4) =>
+     *         0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 => 55
+     *         0, 1, 1, 2 => 2
+     *         return 2 + 55 => 57
+     *    0 FibonacciSum(5)
+     *         0, 1, 1, 2, 3 =>  3
+     *         return 3
+     * 
+    */
+
+    private int FibonacciSum(params int[] numbers)
+    {
+
+        return 0;
+    }
+
+    // =========================== DON'T EDIT BELOW THIS LINE =========================== //
+
+    public TextMeshProUGUI isRectangleTest;
+
+    private void Update()
+    {
+        isRectangleTest.text = "Is Rectangle Assignment\n<align=left>\n";
+        isRectangleTest.text += Success(FibonacciSum(2, 5, 9) == 25) + " works for numbers in order.\n";
+        isRectangleTest.text += Success(FibonacciSum(11, 4) == 57) + " works for number out of order.\n";
+        isRectangleTest.text += Success(FibonacciSum(5) == 3) + " works for one number.\n";
+        
+    }
+
+    private string Success(bool test)
+    {
+        return test ? "<color=\"green\">PASS</color>" : "<color=\"red\">FAIL</color>";
+    }
+}
